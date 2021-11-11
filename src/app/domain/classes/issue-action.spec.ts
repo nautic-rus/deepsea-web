12bcf4a79bbc40b76540d36b7c9f9a71be66a42d
bogdan <Whatab0utus>
2021-11-11 14:34:58 +0300

    some changes

---
+++ src/app/domain/classes/issue-action.spec.ts
@@ -0,0 +1,7 @@
+import { IssueAction } from './issue-action';
+
+describe('IssueAction', () => {
+  it('should create an instance', () => {
+    expect(new IssueAction()).toBeTruthy();
+  });
+});
---
+++ src/app/domain/classes/issue-action.ts
@@ -0,0 +1,4 @@
+export class IssueAction {
+  action: string = '';
+  rule: string = '';
+}
