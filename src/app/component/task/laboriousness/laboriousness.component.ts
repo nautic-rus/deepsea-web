69fb55f4bc1918f7df8e8e4e767a5f073039a82a
bogdan <Whatab0utus>
2021-12-07 10:36:02 +0300

    some changes

--- src/app/component/task/laboriousness/laboriousness.component.ts
+++ src/app/component/task/laboriousness/laboriousness.component.ts
@@ -5,6 +5,7 @@ import {LanguageService} from "../../../domain/language.service";
 import {IssueManagerService} from "../../../domain/issue-manager.service";
 import {AuthManagerService} from "../../../domain/auth-manager.service";
 import {Issue} from "../../../domain/classes/issue";
+import {MessageService} from "primeng/api";
 
 @Component({
   selector: 'app-laboriousness',
@@ -14,16 +15,17 @@ import {Issue} from "../../../domain/classes/issue";
 export class LaboriousnessComponent implements OnInit {
 
   hoursAmount = '';
-  calendarDay = '';
+  calendarDay: Date = new Date();
   comment = '';
   today: Date = new Date();
-  constructor(public t: LanguageService, public ref: DynamicDialogRef, private issues: IssueManagerService, private auth: AuthManagerService, public conf: DynamicDialogConfig) { }
+  constructor(public t: LanguageService, public ref: DynamicDialogRef, private issues: IssueManagerService, private auth: AuthManagerService, public conf: DynamicDialogConfig, private messageService: MessageService) { }
 
   ngOnInit(): void {
   }
   commit(){
     this.issues.setIssueLabor(this.auth.getUser().login, (this.conf.data as Issue).id, +this.hoursAmount, this.comment, +this.calendarDay).then(res => {
-      this.ref.close('success');
+      this.messageService.add({key:'task', severity:'success', summary:'Set Labor', detail:'You have successfully updated issue labor.'});
+      //this.ref.close('success');
     });
   }
   close() {
