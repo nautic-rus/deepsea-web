dc3794e092dffb2a25928821285e63343c5dd0cb
bogdan <Whatab0utus>
2021-12-25 19:48:43 +0300

    some changes

--- src/app/component/tools/trays-by-zones-and-systems/generation-wait/generation-wait.component.ts
+++ src/app/component/tools/trays-by-zones-and-systems/generation-wait/generation-wait.component.ts
@@ -55,4 +55,38 @@ export class GenerationWaitComponent implements OnInit {
   openFile(file: string) {
     window.open(file);
   }
+  getFileExtensionIcon(file: string) {
+    switch (file.toLowerCase().split('.').pop()){
+      case 'pdf': return 'pdf.svg';
+      case 'dwg': return 'dwg.svg';
+      case 'xls': return 'xls.svg';
+      case 'xlsx': return 'xls.svg';
+      case 'doc': return 'doc.svg';
+      case 'docx': return 'doc.svg';
+      case 'png': return 'png.svg';
+      case 'jpg': return 'jpg.svg';
+      case 'txt': return 'txt.svg';
+      case 'zip': return 'zip.svg';
+      case 'mp4': return 'mp4.svg';
+      default: return 'file.svg';
+    }
+  }
+  trimFileName(input: string, length: number = 10): string{
+    let split = input.split('.');
+    let name = split[0];
+    let extension = split[1];
+    if (name.length > length){
+      return name.substr(0, length - 2) + '..' + name.substr(name.length - 2, 2) + '.' + extension;
+    }
+    else{
+      return input;
+    }
+  }
+  getDate(dateLong: number): string{
+    if (dateLong == 0){
+      return '--/--/----';
+    }
+    let date = new Date(dateLong);
+    return ('0' + date.getDate()).slice(-2) + "." + ('0' + (date.getMonth() + 1)).slice(-2) + "." + date.getFullYear();
+  }
 }
