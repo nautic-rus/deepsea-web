b6e566bbf90979789b33b07fc6ec83bf9d337292
bogdan <Whatab0utus>
2022-02-16 11:42:45 +0300

    some changes

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -351,7 +351,7 @@ export class HomeComponent implements OnInit, AfterContentChecked {
     let states = [];
     states.push(issue.ready[0] == '1' ? 'М' : '-');
     states.push(issue.ready[1] == '1' ? 'Ч' : '-');
-    states.push(issue.ready[2] == '1' ? 'Н' : '-');
+    states.push(issue.ready[2] == '1' ? 'Р' : '-');
     issue.readyM = issue.ready[0] == '1';
     issue.readyD = issue.ready[1] == '1';
     issue.readyN = issue.ready[2] == '1';
