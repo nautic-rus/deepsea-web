d7a75e9570759650e095f576d565cdcbf2222985
spiridovichp <spiridovich.p@gmail.com>
2022-03-04 21:20:09 +0300

    ok

---
+++ src/app/component/home-mobile/home-mobile.module.ts
@@ -0,0 +1,20 @@
+// Angular Imports
+import { NgModule } from '@angular/core';
+
+// This Module's Components
+import { HomeMobileComponent } from './home-mobile.component';
+
+@NgModule({
+    imports: [
+
+    ],
+    declarations: [
+        HomeMobileComponent,
+    ],
+    exports: [
+        HomeMobileComponent,
+    ]
+})
+export class HomeMobileModule {
+
+}
