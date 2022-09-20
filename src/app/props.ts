6961aeb362b06476dcf6320bd1fadfa1cc1b2df3
bogdan <Whatab0utus>
2022-09-20 14:57:51 +0300

    some changes

--- src/app/props.ts
+++ src/app/props.ts
@@ -5,7 +5,7 @@ export const http = `https://${origin}/rest`;
 export const http1 = 'http://192.168.1.122:1112';
 
 export const httpSpec = `https://${origin}/rest-spec`;
-export const httpSpec1 = `http://192.168.1.185:1113`;
+export const httpSpec1 = `http://192.168.1.122:1113`;
 export const httpSpecDebug = 'http://192.168.1.122:1113';
 
 
