21313a4c65c0dcf61665c2afb41ebe81e7960d2e
bogdan <Whatab0utus>
2022-10-25 14:59:57 +0300

    some changes

--- src/app/props.ts
+++ src/app/props.ts
@@ -2,7 +2,7 @@ export const origin = 'deep-sea.ru'
 export const baseUrl = `https://${origin}/`;
 
 export const http = `https://${origin}/rest`;
-export const http1= 'http://192.168.1.122:1112';
+export const http1 = 'http://192.168.1.122:1112';
 
 export const rest = `https://${origin}/rest`;
 
