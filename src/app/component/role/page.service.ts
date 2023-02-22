d76b59d8c6f41494b1b65c63a883e1f59dcbc834
mamonov <SharrkSummoner>
2023-02-22 10:13:49 +0300

    adminconsole

---
+++ src/app/component/role/page.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import * as props from "../../props";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PageService {
+
+  constructor(private http: HttpClient) { }
+
+  getPages() {
+    return this.http.get<any>(props.http + '/pages');
+  }
+}
