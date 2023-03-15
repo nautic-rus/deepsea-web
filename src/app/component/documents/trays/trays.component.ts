e951f019fa8f12c310e301953edfe1037dae0980
mamonov <SharrkSummoner>
2023-03-15 11:13:39 +0300

    some changes

--- src/app/component/documents/trays/trays.component.ts
+++ src/app/component/documents/trays/trays.component.ts
@@ -58,6 +58,86 @@ export class TraysComponent implements OnInit {
   cableBoxesById: any = [];
   tooltips: string[] = [];
   length: number = 0;
+  selectedHeadTab: string = 'Files';
+  fileGroups = [
+    {
+      name: 'Drawings',
+      icon: 'assets/icons/drawings.svg',
+      collapsed: false,
+      need_rights: false
+    },
+    {
+      name: 'Part List',
+      icon: 'assets/icons/files.svg',
+      collapsed: false,
+      need_rights: false
+    }
+  ];
+  selectedRevision = 'PROD';
+  waitForZipFiles = false;
+  dxfEnabled = false;
+  spoolsArchive: any;
+  spoolsArchiveContent: any[] = [];
+  comment = false;
+  message = '';
+  awaitForLoad: string[] = [];
+  loaded: FileAttachment[] = [];
+  // @ts-ignore
+  editor;
+  quillModules =
+    {
+      imageResize: {},
+      clipboard: {
+        matchers: [
+          // @ts-ignore
+          ['img', (node, delta) => {
+            let image = delta.ops[0].insert.image;
+            if ((image.indexOf(";base64")) != -1) {
+              let ext = image.substring("data:image/".length, image.indexOf(";base64"))
+              let fileName = 'clip' + this.generateId(8) + '.' + ext;
+              const find = this.loaded.find(x => x.name == fileName);
+              if (find != null) {
+                this.loaded.splice(this.loaded.indexOf(find), 1);
+              }
+              this.awaitForLoad.push(fileName);
+              this.appRef.tick();
+              fetch(image).then(res => res.blob()).then(blob => {
+                const file = new File([blob], fileName, {type: "image/png"});
+                this.issueManager.uploadFile(file, this.auth.getUser().login).then(res => {
+                  this.loaded.push(res);
+                  this.appRef.tick();
+                  this.message += '<img style="cursor: pointer" src="' + res.url + '"/>';
+                  this.appRef.tick();
+                });
+              });
+              return new Delta();
+            } else {
+              return delta;
+            }
+            //return delta;
+          }]
+        ]
+      },
+      keyboard: {
+        bindings: {
+          tab: {
+            key: 9,
+            handler: function () {
+              return true;
+            }
+          }
+        }
+      }
+    }
+  messageFilter = 'all';
+  showImages = false;
+  // @ts-ignore
+  @ViewChild('img') img;
+  image = '';
+  // @ts-ignore
+  wz;
+
+
 
   constructor(public trayService: TrayService, public device: DeviceDetectorService, public auth: AuthManagerService, private route: ActivatedRoute, private router: Router, private s: SpecManagerService, public l: LanguageService, public issueManager: IssueManagerService, private dialogService: DialogService, private appRef: ApplicationRef) {
   }
@@ -154,10 +234,10 @@ export class TraysComponent implements OnInit {
   getGroupLength(group: any[]) {
     let res = 0;
     console.log(group);
-     group.forEach(x => {
-       res += x.length;
-     });
-     return res;
+    group.forEach(x => {
+      res += x.length;
+    });
+    return res;
   }
 
   getGroupWeight(group: any[]) {
@@ -200,6 +280,407 @@ export class TraysComponent implements OnInit {
     }).onClose.subscribe(() => {
       this.fillRevisions();
     });
+  }
+
+  newImportantMessage() {
+    let res = 0;
+    let completed = false;
+    _.sortBy(this.getMessages(this.issue), x => x.date).reverse().forEach(msg => {
+      if (msg.to_be_replied == 1 && msg.author != this.auth.getUser().login && !completed) {
+        res += 1;
+      } else {
+        completed = true;
+      }
+    });
+    return res;
+  }
+
+  getMessages(issue: Issue) {
+    let res: any[] = [];
+    issue.messages.filter(x => x.prefix == 'turkey').forEach(x => res.push(x));
+    //issue.history.forEach(x => res.push(x));
+    // @ts-ignore
+    return _.sortBy(res, x => x.date != null ? x.date : x.update_date).reverse();
+  }
+
+  getArchive() {
+    return _.sortBy(this.issue.archive_revision_files, x => x.removed_date).reverse();
+  }
+
+  getFileExtensionIcon(file: string) {
+    switch (file.toLowerCase().split('.').pop()) {
+      case 'pdf':
+        return 'pdf.svg';
+      case 'dwg':
+        return 'dwg.svg';
+      case 'xls':
+        return 'xls.svg';
+      case 'xlsx':
+        return 'xls.svg';
+      case 'doc':
+        return 'doc.svg';
+      case 'docx':
+        return 'doc.svg';
+      case 'png':
+        return 'png.svg';
+      case 'jpg':
+        return 'jpg.svg';
+      case 'txt':
+        return 'txt.svg';
+      case 'zip':
+        return 'zip.svg';
+      case 'mp4':
+        return 'mp4.svg';
+      default:
+        return 'file.svg';
+    }
+  }
+
+  trimFileName(input: string, length: number = 10): string {
+    let split = input.split('.');
+    let name = split[0];
+    let extension = split[1];
+    if (name.length > length) {
+      return name.substr(0, length - 2) + '..' + name.substr(name.length - 2, 2) + '.' + extension;
+    } else {
+      return input;
+    }
+  }
+
+  getRevisionFilesOfGroup(fileGroup: string, revision: string): FileAttachment[] {
+    return _.sortBy(this.issue.revision_files.filter(x => (x.group == fileGroup || fileGroup == 'all') && x.revision == revision), x => x.upload_date + x.name).reverse();
+  }
+
+  addFilesToGroup(file_group: string, revision: string) {
+    this.dialogService.open(UploadRevisionFilesComponent, {
+      showHeader: false,
+      modal: true,
+      data: [this.issue.id, file_group]
+    }).onClose.subscribe(res => {
+      this.issueManager.getIssueDetails(this.issue.id).then(issue => {
+        this.issue = issue;
+        this.fillRevisions();
+      });
+    });
+  }
+
+  downloadFiles(group: string, revision: string) {
+    let files = this.getRevisionFilesOfGroup(group, revision);
+    let zipped: string[] = [];
+    this.waitForZipFiles = true;
+    Promise.all(files.map(x => fetch(x.url))).then(blobs => {
+      let zip = new JSZip();
+      blobs.forEach(blob => {
+        // @ts-ignore
+        let name: string = blob.url.split('/').pop();
+        while (zipped.includes(name)) {
+          name = name.split('.').reverse().pop() + '$.' + name.split('.').pop();
+        }
+        zipped.push(name);
+        zip.file(name, blob.blob());
+      });
+      zip.generateAsync({type: "blob"}).then(res => {
+        this.waitForZipFiles = false;
+        saveAs(res, this.issue.doc_number + '-' + new Date().getTime() + '.zip');
+      });
+    });
+  }
+
+  clearFiles(fileGroup: string, revision: string) {
+    this.dialogService.open(ClearFilesComponent, {
+      showHeader: false,
+      modal: true,
+      data: this.issue
+    }).onClose.subscribe(res => {
+      if (res == 'success') {
+        this.issueManager.clearRevisionFiles(this.issue.id, this.auth.getUser().login, fileGroup, revision).then(() => {
+          this.fillRevisions();
+        });
+      }
+    });
+  }
+
+  getDate(dateLong: number): string {
+    if (dateLong == 0) {
+      return '--/--/----';
+    }
+    let date = new Date(dateLong);
+    return ('0' + date.getDate()).slice(-2) + "." + ('0' + (date.getMonth() + 1)).slice(-2) + "." + date.getFullYear();
+  }
+
+  showDxfInViewer(url: string) {
+    if (!this.dxfEnabled) {
+      this.dxfEnabled = !this.dxfEnabled;
+    }
+    this.router.navigate([], {
+      queryParams: {dxf: null, searchSpool: null, searchNesting: null},
+      queryParamsHandling: 'merge'
+    }).then(() => {
+      // @ts-ignore
+      this.router.navigate([], {
+        queryParams: {dxf: url, searchSpool: null, searchNesting: null},
+        queryParamsHandling: 'merge'
+      });
+    });
+  }
+
+  openFile(file: FileAttachment) {
+    window.open(file.url, '_blank');
+  }
+
+  deleteFile(fileUrl: string) {
+    this.dialogService.open(ClearFilesComponent, {
+      showHeader: false,
+      modal: true,
+      data: this.issue
+    }).onClose.subscribe(res => {
+      if (res == 'success') {
+        this.issueManager.deleteRevisionFile(fileUrl, this.auth.getUser().login).then(() => {
+          this.fillRevisions();
+        });
+      }
+    });
+  }
+
+  showDxfInViewerZip(zip: FileAttachment, file: string) {
+    fetch(zip.url).then(response => response.blob()).then(blob => {
+      JSZip.loadAsync(blob).then(res => {
+        let findSpool = Object.keys(res.files).find(x => x.includes(file));
+        if (findSpool != null) {
+          res.files[findSpool].async('string').then(fileBlob => {
+            let blob = new Blob([fileBlob], {type: 'text/plain'});
+            let fileUpload = new File([blob], file);
+            this.issueManager.uploadFile(fileUpload, zip.author, zip.name).then(fileAttachment => {
+              if (!this.dxfEnabled) {
+                this.dxfEnabled = !this.dxfEnabled;
+              }
+              this.router.navigate([], {
+                queryParams: {dxf: null, searchSpool: null, searchNesting: null},
+                queryParamsHandling: 'merge'
+              }).then(() => {
+                // @ts-ignore
+                this.router.navigate([], {
+                  queryParams: {
+                    dxf: fileAttachment.url,
+                    searchSpool: null,
+                    searchNesting: null
+                  }, queryParamsHandling: 'merge'
+                });
+              });
+            });
+          });
+        }
+      });
+    });
+  }
+
+  downloadFileFromZip(zip: FileAttachment, file: string) {
+    fetch(zip.url).then(response => response.blob()).then(blob => {
+      JSZip.loadAsync(blob).then(res => {
+        let findSpool = Object.keys(res.files).find(x => x.includes(file));
+        if (findSpool != null) {
+          res.files[findSpool].async('string').then(fileBlob => {
+            let blob = new Blob([fileBlob], {type: 'text/plain'});
+            const element = document.createElement('a');
+            element.setAttribute('href', URL.createObjectURL(blob));
+            element.setAttribute('download', file);
+            element.style.display = 'none';
+            document.body.appendChild(element);
+            element.click();
+            document.body.removeChild(element);
+          });
+        }
+      });
+    });
+  }
+
+  showComment() {
+    this.comment = true;
+    this.message = '';
+    this.awaitForLoad = [];
+    this.loaded = [];
+    setTimeout(() => {
+      this.editor.focus();
+    })
+  }
+
+  onEditorPressed(event: KeyboardEvent) {
+    if (event.ctrlKey && event.key === 'Enter') {
+      this.sendMessage();
+    }
+  }
+
+  editorInit(event: any) {
+    this.editor = event;
+  }
+
+  sendMessage() {
+    let message = new IssueMessage();
+    message.author = this.auth.getUser().login;
+    message.content = this.message;
+    message.file_attachments = this.loaded;
+    message.prefix = 'turkey';
+    message.to_be_replied = this.auth.hasPerms('needs-reply') ? 1 : 0;
+    // @ts-ignore
+    this.issueManager.setIssueMessage(this.issue.id, message).then(res => {
+      this.issueManager.getIssueDetails(this.issue.id).then(issue => {
+        this.issue = issue;
+      });
+    });
+    this.comment = false;
+  }
+  handleImageInput(files: FileList | null) {
+    if (files != null){
+      for (let x = 0; x < files.length; x++){
+        let file = files.item(x);
+        if (file != null){
+          // @ts-ignore
+          const find = this.loaded.find(x => x.name == file.name);
+          if (find != null){
+            this.loaded.splice(this.loaded.indexOf(find), 1);
+          }
+          this.awaitForLoad.push(file.name);
+        }
+      }
+      for (let x = 0; x < files.length; x++){
+        let file = files.item(x);
+        if (file != null){
+          this.issueManager.uploadFile(file, this.auth.getUser().login).then(res => {
+            this.message += '<img style="cursor: pointer" src="' + res.url + '"/>';
+            this.loaded.push(res);
+          });
+        }
+      }
+    }
+  }
+  generateId(length: number): string {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() *
+        charactersLength));
+    }
+    return result;
+  }
+  handleFileInput(files: FileList | null) {
+    if (files != null){
+      for (let x = 0; x < files.length; x++){
+        let file = files.item(x);
+        if (file != null){
+          // @ts-ignore
+          const find = this.loaded.find(x => x.name == file.name);
+          if (find != null){
+            this.loaded.splice(this.loaded.indexOf(find), 1);
+          }
+          this.awaitForLoad.push(file.name);
+        }
+      }
+      for (let x = 0; x < files.length; x++){
+        let file = files.item(x);
+        if (file != null){
+          this.issueManager.uploadFile(file, this.auth.getUser().login).then(res => {
+            this.loaded.push(res);
+          });
+        }
+      }
+    }
+  }
+  isLoaded(file: string) {
+    return this.loaded.find(x => x.name == file);
+  }
+  remove(file: string) {
+    let find = this.loaded.find(x => x.name == file);
+    if (find != null){
+      this.loaded.splice(this.loaded.indexOf(find), 1);
+    }
+    let findAwait = this.awaitForLoad.find(x => x == file);
+    if (findAwait != null){
+      this.awaitForLoad.splice(this.awaitForLoad.indexOf(findAwait), 1);
+    }
+  }
+  openUserInfo(author: string) {
+    this.dialogService.open(UserCardComponent, {
+      showHeader: false,
+      modal: true,
+      data: author
+    });
+  }
+  localeGender(userId: string){
+    let find = this.auth.users.find(x => x.login == userId);
+    return find != null && find.gender == 'female' && this.l.language == 'ru' ? 'а' : '';
+  }
+  editorClicked(event: any) {
+
+    event.preventDefault();
+    event.stopPropagation();
+    if (event.target.localName == 'img'){
+      this.showImage(event.target.currentSrc);
+    }
+    else if (event.target.localName == 'a'){
+      window.open(event.target.href, '_blank');
+    }
+  }
+  showImage(url: string){
+    this.image = url;
+    this.showImages = true;
+    setTimeout(() => {
+      if (this.wz == null){
+        this.wz = mouseWheelZoom({
+          // @ts-ignore
+          element: document.querySelector('[data-wheel-zoom]'),
+          zoomStep: .25
+        });
+      }
+      this.wz.setSrcAndReset(url);
+    });
+  }
+  onEditorDrop(event: any) {
+    event.preventDefault();
+    let files = event.dataTransfer.files;
+    if (files != null){
+      for (let x = 0; x < files.length; x++){
+        let file = files.item(x);
+        if (file != null){
+          const acceptedImageTypes = ['.jpg', '.jpeg', '.png'];
+          let isImage = false;
+          acceptedImageTypes.forEach(x => {
+            if (file.name.includes(x)){
+              isImage = true;
+            }
+          });
+          if (isImage) {
+            const find = this.loaded.find(x => x.name == file.name);
+            if (find != null) {
+              this.loaded.splice(this.loaded.indexOf(find), 1);
+            }
+            this.awaitForLoad.push(file.name);
+            this.issueManager.uploadFile(file, this.auth.getUser().login).then(res => {
+              this.message += '<img style="cursor: pointer" src="' + res.url + '"/>';
+              this.loaded.push(res);
+            });
+          }
+        }
+      }
+    }
+  }
+  getAuthor(author: string) {
+    return this.auth.getUserName(author);
+  }
+  getNoneZeroResult(input: string) {
+    return input.length == 0 ? '<div class="text-none">Нет</div>' : input;
+  }
+  getNoneZeroInput(input: string) {
+    return input == '-' ? '<div class="text-none">Нет</div>' : input;
+  }
+  getDateNoTime(dateLong: number): string{
+    if (dateLong == 0) {
+      return '';
+    }
+    else{
+      let date = new Date(dateLong);
+      return ('0' + date.getDate()).slice(-2) + "." + ('0' + (date.getMonth() + 1)).slice(-2) + "." + date.getFullYear();
+    }
   }
 
   setCols() {
