689094519fe3bf596d7b2fd8885fc04ed353d73e
mamonov <SharrkSummoner>
2023-03-16 16:06:56 +0300

    some changes

--- src/app/component/documents/trays/trays.component.ts
+++ src/app/component/documents/trays/trays.component.ts
@@ -32,6 +32,7 @@ import {
 import * as XLSX from "xlsx";
 import {HullEspGenerationWaitComponent} from "../hull-esp/hull-esp-generation-wait/hull-esp-generation-wait.component";
 import {TrayEspGenerationWaitComponent} from "./tray-esp-generation-wait/tray-esp-generation-wait.component";
+import {forkJoin} from "rxjs";
 
 @Component({
   selector: 'app-trays',
@@ -60,6 +61,33 @@ export class TraysComponent implements OnInit {
   cableBoxesByCode: any = [];
   cableBoxesById: any = [];
   tooltips: string[] = [];
+  selectedHeadTab: string = 'Files';
+  fileGroups = [
+    {
+      name: 'Drawings',
+      icon: 'assets/icons/drawings.svg',
+      collapsed: false,
+      need_rights: false
+    },
+    {
+      name: 'Part List',
+      icon: 'assets/icons/files.svg',
+      collapsed: false,
+      need_rights: false
+    }
+  ];
+  cloudDate = false;
+  selectedRevision = 'PROD';
+  fileSort = 'name';
+  sortReverse = false;
+  waitForZipFiles = false;
+  cmapuser = '';
+  dxfEnabled = false;
+  cmapdate = 0;
+  cmapFormat = '';
+  cutEnabled = false;
+  cmap = '';
+  miscIssues: Issue[] = [];
 
 
   constructor(public trayService: TrayService, public device: DeviceDetectorService, public auth: AuthManagerService, private route: ActivatedRoute, private router: Router, private s: SpecManagerService, public l: LanguageService, public issueManager: IssueManagerService, private dialogService: DialogService, private appRef: ApplicationRef) {
@@ -84,30 +112,15 @@ export class TraysComponent implements OnInit {
     this.issueRevisions.splice(0, this.issueRevisions.length);
     this.issueManager.getIssueDetails(this.issueId).then(res => {
       this.issue = res;
-      this.issueRevisions.push(this.issue.revision);
-      this.issue.revision_files.map(x => x.revision).forEach(gr => {
-        if (!this.issueRevisions.includes(gr)) {
-          this.issueRevisions.push(gr);
-        }
-      });
-      this.issueRevisions = _.sortBy(this.issueRevisions, x => x).reverse();
-
-      let findSpools = this.issue.revision_files.find(x => x.group == 'Pipe Spools' && x.name.includes('.zip'));
-      if (findSpools != null) {
-        this.traysArchive = findSpools;
-        this.traysArchiveContent.splice(0, this.traysArchiveContent.length);
-        fetch(findSpools.url).then(response => response.blob()).then(blob => {
-          JSZip.loadAsync(blob).then(res => {
-            Object.keys(res.files).forEach(file => {
-              let name = file.split('/');
-              if (name.length > 1 && name[1] != '') {
-                this.traysArchiveContent.push(name[1]);
-              }
-            });
-          });
+      this.miscIssues.splice(0, this.miscIssues.length);
+      this.issueManager.getIssues('op').then(issues => {
+        issues.filter(x => x.doc_number == this.issue.doc_number).forEach(x => this.miscIssues.push(x));
+        this.miscIssues.forEach(x => {
+          issues.filter(y => y.parent_id == x.id).forEach(ch => {
+            this.miscIssues.push(ch);
+          })
         });
-
-      }
+      });
 
       this.fillTrays();
       this.fillCableBoxes();
@@ -208,6 +221,337 @@ export class TraysComponent implements OnInit {
   exportSketches() {
   //
   }
+
+  getRevisionFilesOfGroupAux(fileGroup: string, revision: string): FileAttachment[] {
+    let files = this.issue.revision_files.filter(x => (x.group == fileGroup || fileGroup == 'all') && x.revision == revision);
+    if (this.fileSort == 'name'){
+      return this.sortReverse ? _.sortBy(files, x => x.name).reverse() : _.sortBy(files, x => x.name);
+    }
+    else {
+      return this.sortReverse ? _.sortBy(files, x => x.upload_date).reverse() : _.sortBy(files, x => x.upload_date);
+    }
+  }
+
+  getRevisionFilesOfGroup(fileGroup: string, revision: string): FileAttachment[] {
+    if (this.issue.cloud_files.length == 0){
+      return this.getRevisionFilesOfGroupAux(fileGroup, revision);
+    }
+    this.cloudDate = true;
+    let files = this.issue.cloud_files.filter(x => (x.group == fileGroup || fileGroup == 'all'));
+    if (this.fileSort == 'name'){
+      return this.sortReverse ? _.sortBy(files, x => x.name).reverse() : _.sortBy(files, x => x.name);
+    }
+    else {
+      return this.sortReverse ? _.sortBy(files, x => x.upload_date).reverse() : _.sortBy(files, x => x.upload_date);
+    }
+  }
+
+  addFilesToGroup(file_group: string, revision: string) {
+    this.dialogService.open(UploadRevisionFilesComponent, {
+      showHeader: false,
+      modal: true,
+      data: [this.issue.id, file_group]
+    }).onClose.subscribe(res => {
+      this.issueManager.getIssueDetails(this.issue.id).then(issue => {
+        this.issue = issue;
+        this.fillRevisions();
+      });
+    });
+  }
+
+  downloadFiles(group: string, revision: string, format = 'cnc') {
+    if (group == 'Cutting Map'){
+      this.downloadFilesCVC(group, revision, format);
+      return;
+    }
+    let files = this.getRevisionFilesOfGroup(group, revision);
+    let zipped: string[] = [];
+    this.waitForZipFiles = true;
+    Promise.all(files.map(x => fetch(x.url))).then(blobs => {
+      let zip = new JSZip();
+      blobs.forEach(blob => {
+        // @ts-ignore
+        let name: string = blob.url.split('/').pop();
+        while (zipped.includes(name)){
+          name = name.split('.').reverse().pop() + '$.' + name.split('.').pop();
+        }
+        zipped.push(name);
+        zip.file(name, blob.blob());
+      });
+      zip.generateAsync({type:"blob"}).then(res => {
+        this.waitForZipFiles = false;
+        saveAs(res, this.issue.doc_number + '-' + new Date().getTime() + '.zip');
+      });
+    });
+  }
+
+  downloadFilesCVC(group: string, revision: string, format = 'cnc') {
+    let files = this.getRevisionFilesOfGroup(group, revision);
+    let zipped: string[] = [];
+    this.waitForZipFiles = true;
+    forkJoin(files.map(x => fetch(x.url))).subscribe(blobs => {
+      forkJoin(blobs.map(file => file.text())).subscribe(texts => {
+
+        if (format == 'txt'){
+          let zip = new JSZip();
+          texts.forEach(txt => {
+            // @ts-ignore
+            let name: string = blobs[texts.indexOf(txt)].url.split('/').pop();
+            while (zipped.includes(name)) {
+              name = name.split('.').reverse().pop() + '$.' + name.split('.').pop();
+            }
+            name = name.replace('C-' + this.project + '-', '');
+            // @ts-ignore
+            zip.file(name, txt);
+          });
+          zip.generateAsync({type: "blob"}).then(res => {
+            this.waitForZipFiles = false;
+            saveAs(res, this.issue.doc_number + '-' + new Date().getTime() + '.zip');
+          });
+          return;
+        }
+
+        forkJoin(texts.map(text => {
+          this.cmapuser = this.auth.getUserName(files[texts.indexOf(text)].author);
+          this.cmapdate = files[texts.indexOf(text)].upload_date;
+          if (format == 'essi'){
+            return this.s.createESSI(text.split('\n'), this.cmapuser + ' at ' + new Date(this.cmapdate).toDateString());
+          }
+          else if (format == 'tap'){
+            return this.s.createTAP(text.split('\n'), this.cmapuser + ' at ' + new Date(this.cmapdate).toDateString());
+          }
+          else{
+            return this.s.createCNC(text.split('\n'), this.cmapuser + ' at ' + new Date(this.cmapdate).toDateString());
+          }
+        })).subscribe(cncs => {
+          let zip = new JSZip();
+          cncs.forEach(cnc => {
+            // @ts-ignore
+            let name: string = blobs[cncs.indexOf(cnc)].url.split('/').pop();
+            while (zipped.includes(name)){
+              name = name.split('.').reverse().pop() + '$.' + name.split('.').pop();
+            }
+
+            if (format == 'essi'){
+              name = name.replace('.txt', '.ESI');
+            }
+            else if (format == 'tap'){
+              name = name.replace('.txt', '.TAP');
+            }
+            else{
+              name = name.replace('.txt', '.MPG');
+            }
+
+
+            name = name.replace('C-' + this.project + '-', '');
+            zipped.push(name);
+            // @ts-ignore
+            zip.file(name, cnc.join('\n'));
+          });
+          zip.generateAsync({type: "blob"}).then(res => {
+            this.waitForZipFiles = false;
+            saveAs(res, this.issue.doc_number + '-' + new Date().getTime() + '.zip');
+          });
+        });
+      });
+    });
+  }
+
+  setFileSort(value: string) {
+    this.fileSort = value;
+    this.sortReverse = !this.sortReverse;
+  }
+
+  getFileExtensionIcon(file: string) {
+    switch (file.toLowerCase().split('.').pop()){
+      case 'pdf': return 'pdf.svg';
+      case 'dwg': return 'dwg.svg';
+      case 'xls': return 'xls.svg';
+      case 'xlsx': return 'xls.svg';
+      case 'doc': return 'doc.svg';
+      case 'docx': return 'doc.svg';
+      case 'png': return 'png.svg';
+      case 'jpg': return 'jpg.svg';
+      case 'txt': return 'txt.svg';
+      case 'zip': return 'zip.svg';
+      case 'mp4': return 'mp4.svg';
+      default: return 'file.svg';
+    }
+  }
+
+  trimFileName(input: string, length: number = 10): string{
+    let split = input.split('.');
+    let name = split[0];
+    let extension = split[1];
+    if (name.length > length){
+      return name.substr(0, length - 2) + '..' + name.substr(name.length - 2, 2) + '.' + extension;
+    }
+    else{
+      return input;
+    }
+  }
+
+  getDate(dateLong: number): string{
+    if (this.cloudDate){
+      return this.getCloudDateNoTime(dateLong);
+    }
+    if (dateLong == 0){
+      return '--/--/----';
+    }
+    let date = new Date(dateLong);
+    return ('0' + date.getDate()).slice(-2) + "." + ('0' + (date.getMonth() + 1)).slice(-2) + "." + date.getFullYear();
+  }
+
+  getCloudDateNoTime(dateLong: number): string{
+    if (dateLong == 0) {
+      return '';
+    }
+    else{
+      let date = new Date(0);
+      date.setUTCSeconds(dateLong);
+      return ('0' + date.getDate()).slice(-2) + "." + ('0' + (date.getMonth() + 1)).slice(-2) + "." + date.getFullYear();
+    }
+  }
+
+  showDxfInViewer(url: string) {
+    if (!this.dxfEnabled){
+      this.dxfEnabled = !this.dxfEnabled;
+    }
+    this.router.navigate([], {queryParams: {dxf: null, search: null, searchNesting: null}, queryParamsHandling: 'merge'}).then(() => {
+      // @ts-ignore
+      this.router.navigate([], {queryParams: {dxf: url, search: null, searchNesting: null}, queryParamsHandling: 'merge'});
+    });
+  }
+
+  showCuttingFile(file: FileAttachment) {
+    console.log(file);
+    this.cmap = file.url;
+    this.dxfEnabled = false;
+    this.cutEnabled = false;
+    this.router.navigate([], {queryParams: {cmap: null, cmapuser: null, cmapdate: null}, queryParamsHandling: 'merge'}).then(() => {
+      setTimeout(() => {
+        this.cutEnabled = true;
+        console.log(this.cutEnabled);
+        // @ts-ignore
+        this.router.navigate([], {queryParams: {cmap: file.url, cmapuser: this.auth.getUserName(file.author), cmapdate: file.upload_date}, queryParamsHandling: 'merge'});
+        const style = document.createElement('style');
+        style.innerHTML = `
+          .editBlock pre {
+            height: 42vh !important;
+          }
+          .viewContainer .TwoDView {
+            height: 45vh !important;
+          }
+        `;
+        document.head.appendChild(style);
+      });
+    });
+  }
+
+  openFile(file: FileAttachment, cmapFormat = 'cnc') {
+    if (file.group == 'Cutting Map'){
+      this.cmap = file.url;
+      this.cmapFormat = cmapFormat;
+      this.cmapuser = this.auth.getUserName(file.author);
+      this.downloadOpenedCMAP();
+    }
+    else{
+      window.open(file.url, '_blank');
+    }
+  }
+
+  deleteFile(fileUrl: string){
+    this.dialogService.open(ClearFilesComponent, {
+      showHeader: false,
+      modal: true,
+      data: this.issue
+    }).onClose.subscribe(res => {
+      if (res == 'success'){
+        this.issueManager.deleteRevisionFile(fileUrl, this.auth.getUser().login).then(() => {
+          this.fillRevisions();
+        });
+      }
+    });
+  }
+
+  downloadOpenedCMAP() {
+    if (this.cmap != ''){
+      fetch(this.cmap).then(res => {
+        res.text().then(text => {
+          if (this.cmapFormat == 'cnc'){
+            this.s.createCNC(text.split('\n'), this.cmapuser + ' at ' + new Date(this.cmapdate).toDateString()).then(res => {
+
+              var element = document.createElement('a');
+              element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(res.join('\n')));
+              // @ts-ignore
+              element.setAttribute('download', this.cmap.split('/').pop().replace('C-' + this.project + '-', '').replace('.txt', '.MPG'));
+
+              element.style.display = 'none';
+              document.body.appendChild(element);
+
+              element.click();
+
+              document.body.removeChild(element);
+            });
+          }
+          else if (this.cmapFormat == 'tap'){
+            this.s.createTAP(text.split('\n'), this.cmapuser + ' at ' + new Date(this.cmapdate).toDateString()).then(res => {
+
+              var element = document.createElement('a');
+              element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(res.join('\n')));
+              // @ts-ignore
+              element.setAttribute('download', this.cmap.split('/').pop().replace('C-' + this.project + '-', '').replace('.txt', '.TAP'));
+
+              element.style.display = 'none';
+              document.body.appendChild(element);
+
+              element.click();
+
+              document.body.removeChild(element);
+            });
+          }
+          else{
+            this.s.createESSI(text.split('\n'), this.cmapuser + ' at ' + new Date(this.cmapdate).toDateString()).then(res => {
+
+              var element = document.createElement('a');
+              element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(res.join('\n')));
+              // @ts-ignore
+              element.setAttribute('download', this.cmap.split('/').pop().replace('C-' + this.project + '-', '').replace('.txt', '.ESI'));
+              element.style.display = 'none';
+              document.body.appendChild(element);
+
+              element.click();
+
+              document.body.removeChild(element);
+            });
+          }
+        });
+      });
+    }
+  }
+
+  clearFiles(fileGroup: string, revision: string) {
+    this.dialogService.open(ClearFilesComponent, {
+      showHeader: false,
+      modal: true,
+      data: this.issue
+    }).onClose.subscribe(res => {
+      if (res == 'success'){
+        this.issueManager.clearRevisionFiles(this.issue.id, this.auth.getUser().login, fileGroup, revision).then(() => {
+          this.fillRevisions();
+        });
+      }
+    });
+  }
+
+  openIssue(id: number) {
+    window.open('/?taskId=' + id, '_blank');
+  }
+
+  getArchive() {
+    return _.sortBy(this.issue.archive_revision_files, x => x.removed_date).reverse();
+  }
+
 
 
   setCols() {
