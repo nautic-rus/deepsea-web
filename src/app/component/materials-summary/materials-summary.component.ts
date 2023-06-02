dbd3d06ba95ef0847c3b2bef4a5c723c6e2b412b
redwick <redeeming.fury@gmail.com>
2023-06-02 15:21:37 +0300

    some changes

--- src/app/component/materials-summary/materials-summary.component.ts
+++ src/app/component/materials-summary/materials-summary.component.ts
@@ -332,7 +332,7 @@ export class MaterialsSummaryComponent implements OnInit {
       materialsValue.forEach((m => m.materialCloudDirectory = ''));
       this.materials = materialsValue;
       this.materialsSrc = materialsValue;
-      this.materialManager.getMaterialsSummary(this.project.foran, 'pipe,hull').then(materialsSummary => {
+      this.materialManager.getMaterialsSummary(this.project.foran, 'pipe,hull,device').then(materialsSummary => {
         this.materialsFilled = true;
         this.materialsSummarySrc = materialsSummary;
         this.materialsSummary = this.materialsSummarySrc;
