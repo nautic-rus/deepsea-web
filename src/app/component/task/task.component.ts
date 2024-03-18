f9413390eb365c54de7f583dcaf623928e7a6e08
isaeva <ystropilova@yandex.ru>
2024-03-18 16:49:36 +0300

    commit

--- src/app/component/task/task.component.ts
+++ src/app/component/task/task.component.ts
@@ -1234,7 +1234,7 @@ export class TaskComponent implements OnInit {
     let issue = new Issue();
     issue.parent_id = this.issue.id;
     issue.project = this.issue.project;
-    issue.doc_number = this.issue.doc_number;
+    // issue.doc_number = this.issue.doc_number;
     // issue.name = this.issue.doc_number + ' ' + this.issue.name;
     issue.department = this.issue.department;
     issue.for_revision = this.issue.revision;
@@ -1426,8 +1426,8 @@ export class TaskComponent implements OnInit {
     let issue = new Issue();
     issue.parent_id = this.issue.id;
     issue.project = this.issue.project;
-    issue.doc_number = this.issue.doc_number;
-    issue.name = this.issue.name;
+    // issue.doc_number = this.issue.doc_number;
+    // issue.name = this.issue.name;
     issue.department = this.issue.department;
     issue.for_revision = this.issue.revision;
     this.newTask(issue, 'combine');
