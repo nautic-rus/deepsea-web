b1f1a245aa2da6e0decbc6d11b62471c58a6c575
redwick <redeeming.fury@gmail.com>
2024-09-24 14:14:33 +0300

    some changes

--- src/app/component/task/task.component.ts
+++ src/app/component/task/task.component.ts
@@ -62,7 +62,7 @@ export class TaskComponent implements OnInit {
   comment = false;
   answer = false;
   messageFilter = 'all';
-  issueRevisions = ['-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'F', 'G', 'H', 'I', 'J', 'K'];
+  issueRevisions = ['-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'];
   awaitForLoad: string[] = [];
   loaded: FileAttachment[] = [];
   loadedMain: FileAttachment[] = [];
